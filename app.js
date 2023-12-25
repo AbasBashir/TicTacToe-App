2715db7faa6da3b3dff78656ce01eb28645de866
AbasBashir <MrAbasBashir@gmail.com>
2023-12-25 15:38:32 +0100

    Added funtions to play and check the result

--- app.js
+++ app.js
@@ -66,3 +66,85 @@ for (let i = 1; i < 10; i++) {
     });
 
 }
+
+
+function game(target){
+
+    if (currentPlayer === 'O') {
+
+        // Checking if the grid item has no child, if true, then we create one 
+        if (target.firstElementChild === null) {
+
+            let circle = document.createElement("div");
+    
+            circle.classList.add("circle");
+
+            circle.innerText = 'O';
+    
+            target.appendChild(circle);
+
+            sumCount++;
+            
+        }
+
+        // Change currentplayer once player O has played 
+        currentPlayer = 'X';
+        
+    } else {
+
+        if (target.firstElementChild === null) {
+    
+            let x = document.createElement("div");
+
+            x.classList.add("x");
+
+            x.innerText = 'X';
+    
+            target.appendChild(x);
+    
+            sumCount++;
+
+        }
+    
+        currentPlayer = 'O';
+        
+    }
+    
+    // Check the outcome of the game
+    checkResult(target);
+
+}
+
+function checkResult(target){
+
+    // Storing the original content of the content in the section with .display class before deleting it 
+    let matchConcluded = originalDisplayPlayerParent;
+        
+    for (let i = 0; i < 3; i++) {
+
+        // Checking the outcome of the game for all rows
+        if ((appContainer.children[i * 3].innerText !== '' && appContainer.children[i * 3].innerText === appContainer.children[i * 3 + 1].innerText && appContainer.children[i * 3 + 1].innerText === appContainer.children[i * 3 + 2].innerText) 
+        ){
+            // Display the reset button
+            resetBtn.style.display = 'block';
+
+            // Deleting the <span class="display-player">O</span> tag including its content
+            displayPlayer.remove();
+
+            // Modify the content in the section with .display class
+            matchConcluded.innerText = 'Game Over';
+            matchConcluded.style.fontSize = '30px';
+            matchConcluded.style.color = 'red';
+
+            // Adding a custom message when current player wins 
+            result.innerText = `Player ${target.firstElementChild.innerText} , you've won the game!`;
+            gameOver = true;
+            flagSum = true;
+
+            resetGame(matchConcluded);
+
+        }
+
+    }
+
+}
